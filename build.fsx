91615c3dd9776a8d5a3af4c4676a9c11cfd6a9f8
stuartjdavies <stuart_j_davies@hotmail.com>
2016-04-12 22:01:41 +1000

    ...

---
+++ build.fsx
@@ -0,0 +1,61 @@
+﻿#r @"packages/FAKE/tools/FakeLib.dll"
+
+open Fake
+
+RestorePackages()
+
+// Properties
+let buildDir = "./build/"
+let testDir  = "./test/"
+let deployDir = "./deploy/"
+let toolPath="""C:\Program Files (x86)\NUnit 2.6.4\bin\"""
+
+// version info
+let version = "0.1"  // or retrieve from CI server
+
+// Targets
+Target "Clean" (fun _ ->
+    CleanDirs [buildDir; testDir; deployDir]
+)
+
+Target "BuildApp" (fun _ ->
+   !! "src/**/*.csproj" 
+     |> MSBuildRelease buildDir "Build"
+     |> Log "AppBuild-Output: "
+)
+
+Target "BuildTest" (fun _ ->
+    !! "tests/**/*.csproj"
+      |> MSBuildDebug testDir "Build"
+      |> Log "TestBuild-Output: "
+)
+
+Target "Test" (fun _ ->
+    !! (testDir + "/CRG.Integration.Testing.Tests.dll")
+      |> NUnit (fun p ->
+          {p with ToolPath=toolPath;
+                  DisableShadowCopy = true;
+                  OutputFile = testDir + "TestResults.xml" })
+)
+
+Target "Zip" (fun _ ->
+    !! (buildDir + "/**/*.*")
+        -- "*.zip"
+        |> Zip buildDir (deployDir + "CRG.Test." + version + ".zip")
+)
+
+Target "Default" (fun _ ->
+    trace "Hello World from FAKE"
+)
+
+
+// Dependencies
+"Clean"
+  ==> "BuildApp"
+  ==> "BuildTest"
+//==> "Test"
+  ==> "Zip"
+  ==> "Default"
+
+// start build
+RunTargetOrDefault "Default"
